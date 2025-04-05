9ed4f003097d695062697a831d911a0b224a4d38
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2025-04-06 00:40:52 +0300

    [MOD]: auth routes added to the server

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-const express  = require('express');
-const mongoose = require('mongoose')
-require('dotenv').config({path: '.env.local'});
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: ".env.local" });
 
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 const port = 3000;
 
 mongoose.set("strictQuery", false);
-const mongodb = process.env.DB_URL
-
-app.get('/', (req,res)=> {
-    res.send('hello world')
-});
+const mongodb = process.env.DB_URL;
 
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongodb);
 }
 
-app.listen(port, ()=>{
-    console.log('server is running')
+//Parse JSON and form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Routes
+app.use("/api/auth/", authRoutes);
+
+app.listen(port, () => {
+  console.log("server is running");
 });
