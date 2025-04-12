4092dc5a35db353c05eb5115241f8ca68b0b96f6
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2025-04-13 02:53:07 +0300

    [MOD]: auth middleware added to create task route

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
+
 const {
   createTask,
   updateTask,
   deleteTask,
 } = require("../controllers/task.controller");
 
-router.post("/task/create", createTask);
-router.put("/task/update/?id", updateTask);
-router.delete("/task/delete/?id", deleteTask);
+const { verifyAuth } = require("../middlewares/auth.middleware");
+
+router.post("/create", verifyAuth, createTask);
+router.put("/update/?id", updateTask);
+router.delete("/delete/?id", deleteTask);
 
 module.exports = router;
